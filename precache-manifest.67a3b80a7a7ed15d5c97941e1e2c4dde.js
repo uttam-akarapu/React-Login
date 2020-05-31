dbc402a6cf80c210c4707ef33e0f00fc82f8cd69
Uttam <uttamadeveloper@gmail.com>
2020-05-31 20:31:43 +0530

    Updates

---
+++ precache-manifest.67a3b80a7a7ed15d5c97941e1e2c4dde.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6986e721afbf6d148ef6c1ab034f7b59",
+    "url": "/React-Login/index.html"
+  },
+  {
+    "revision": "368d7257fc61344af9b6",
+    "url": "/React-Login/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "381e0dfeb6be2f333e4a",
+    "url": "/React-Login/static/css/main.7a9d708f.chunk.css"
+  },
+  {
+    "revision": "368d7257fc61344af9b6",
+    "url": "/React-Login/static/js/2.3a89e224.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Login/static/js/2.3a89e224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "381e0dfeb6be2f333e4a",
+    "url": "/React-Login/static/js/main.46ea90e8.chunk.js"
+  },
+  {
+    "revision": "3a16a8f3503bd08addf1",
+    "url": "/React-Login/static/js/runtime-main.b8cacd99.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Login/static/media/logo.5d5d9eef.svg"
+  }
+]);
